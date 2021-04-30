e7cb45f52cbcedd1108563787384370cb842b420
Yong Li <appzone.li@gmail.com>
2021-04-30 10:44:56 +0700

    remove unused code

--- server/src/controller/auth.ts
+++ server/src/controller/auth.ts
@@ -2,30 +2,13 @@ import jwt from "jsonwebtoken";
 import { BaseContext } from "koa";
 import { description, request, summary, tagsAll } from "koa-swagger-decorator";
 import { config } from "../config";
-import { DUMMY_USERS } from "../constants/common";
-import { getUserByEmailPassword, verifyUserByCode } from "../services/authService";
+import { verifyUserByCode } from "../services/authService";
 import { ErrorReason, HttpError } from "../utils/httpError";
 import Response from "../utils/response";
 
 @tagsAll(["Auth"])
 export default class GeneralController {
 
-  @request("post", "/api/auth/login")
-  @summary("Login")
-  @description("Login by providing email and password")
-  public static async login(ctx: BaseContext): Promise<void> {
-    const { email, password } = ctx.request.body;
-    if (!email || !password) {
-      throw new HttpError("Email and password are required", ErrorReason.INVALID_INPUT);
-    }
-    const user = getUserByEmailPassword(email, password);
-    if (!user) {
-      throw new HttpError("Invalid email or password", ErrorReason.UNAUTHORIZED);
-    }
-    const accessToken = jwt.sign(user, config.jwtSecret);
-    const response = new Response({ accessToken });
-    ctx.body = response;
-  }
 
   @request("post", "/api/auth/verify")
   @summary("Verify")
--- server/src/services/authService.ts
+++ server/src/services/authService.ts
@@ -1,15 +1,6 @@
 import { uniqueNamesGenerator, Config, adjectives, colors, animals, names } from "unique-names-generator";
 
-import { DUMMY_USERS } from "../constants/common";
 import { ErrorReason, HttpError } from "../utils/httpError";
-
-const getUserByEmailPassword = (email: string, password: string) => {
-  return DUMMY_USERS.find((x) => x.email === email && password === "1234");
-};
-
-const getUserByUserId = (userId: string) => {
-  return DUMMY_USERS.find((x) => x.id === userId);
-};
 
 const getUser = () => {
   const customConfig: Config = {
@@ -39,6 +30,5 @@ const verifyUserByCode = (code: string | number) => {
 };
 
 export {
-  getUserByEmailPassword,
   verifyUserByCode,
 };
